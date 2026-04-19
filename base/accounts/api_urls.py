801332153769e21679b313af544752157ba786da
Beatriz Machado <beatriz.machado-ext@anbima.com.br>
2026-04-19 04:05:53 -0300

    fix(fcm): import csrf_exempt properly

--- base/accounts/api_urls.py
+++ base/accounts/api_urls.py
@@ -1,4 +1,5 @@
 from django.urls import path, include
+from django.views.decorators.csrf import csrf_exempt
 from rest_framework.routers import DefaultRouter
 from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 from . import api_auth
